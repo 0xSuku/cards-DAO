d76114d42f7c98d98df7e7401bf655b37e241cb1
Suku <unaeternidad@gmail.com>
2023-07-04 23:02:34 -0300

    first poc version of dao cards

---
+++ src/constants/cards.ts
@@ -0,0 +1,22 @@
+export enum CardRanks {
+    Ace = 'Ace',
+    Two = 'Two',
+    Three = 'Three',
+    Four = 'Four',
+    Five = 'Five',
+    Six = 'Six',
+    Seven = 'Seven',
+    Eight = 'Eight',
+    Nine = 'Nine',
+    Ten = 'Ten',
+    Jack = 'Jack',
+    Queen = 'Queen',
+    King = 'King',
+}
+
+export enum CardSuits {
+    Hearts = 'Hearts',
+    Diamonds = 'Diamonds',
+    Clubs = 'Clubs',
+    Spades = 'Spades',
+}
